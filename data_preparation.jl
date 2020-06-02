722a49303ed4bbca9fafe5cffca2ee89aed80c42
Matěj Račinský <matej.racinsky@avast.com>
2020-06-02 13:07:39 +0200

    added way of processing interesting data

--- data_preparation.jl
+++ data_preparation.jl
@@ -17,3 +17,11 @@ delete!(sch2[:behavior][:processes].items.childs, :calls)
 open("src/assets/reddit_cuckoo_schema.json", "w") do f
     write(f, JSON.json(sch2))
 end
+sch2[:behavior].childs
+open("src/assets/reddit_cuckoo_behavior_apistats_schema.json", "w") do f
+    write(f, JSON.json(sch2[:behavior][:apistats]))
+end
+
+open("src/assets/reddit_cuckoo_behavior_summary_schema.json", "w") do f
+    write(f, JSON.json(sch2[:behavior][:summary]))
+end
